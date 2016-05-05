d32adf0a3309e9b76c4a17413c8b08cbae0b4143
Brett Mierzejewski <mierze@gmail.com>
2016-05-04 22:13:36 -0400

    Stripped out ng2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.set('port', (process.env.PORT || 4004));
+app.set('port', (process.env.PORT || 4304));
 
 app.use(express.static(__dirname + '/'));
 
